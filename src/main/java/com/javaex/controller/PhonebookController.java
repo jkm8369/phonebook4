7d9abc364b2e479430cf3121cb0d37a0f0a5a10e
jkm8369 <jkm8369@naver.com>
2025-06-24 17:54:34 +0900

    2025-06-24

--- src/main/java/com/javaex/controller/PhonebookController.java
+++ src/main/java/com/javaex/controller/PhonebookController.java
@@ -1,11 +1,18 @@
 package com.javaex.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.javaex.dao.PhonebookDAO;
+import com.javaex.vo.PhonebookVO;
 
 @Controller
-@RequestMapping("/person")
 public class PhonebookController {
 
 	//필드
@@ -18,18 +25,61 @@ public class PhonebookController {
 	
 	
 	//메소드 일반
-	@RequestMapping(value = "/list", method = {RequestMethod.GET, RequestMethod.POST})
-	public String select() {
-		System.out.println("PhonebookContoroller.select()");
+	@RequestMapping(value ="/list", method = {RequestMethod.GET, RequestMethod.POST})
+	public String list(Model model) {
+		//System.out.println("PhonebookContoroller.list()"); 
 		
-		return "";
+		PhonebookDAO phonebookDAO = new PhonebookDAO();
+		List<PhonebookVO> personList = phonebookDAO.phonbookSelect();
+		
+		//System.out.println(personList); 확인 완
+		
+		model.addAttribute("pList", personList);
+		
+		return "/list";
 	}
 	
-	@RequestMapping(value = "/write", method = {RequestMethod.GET, RequestMethod.POST})
-	public String insert() {
-		System.out.println("PhonebookController.insert()");
-		
-		return "";
+	@RequestMapping(value ="wform", method = {RequestMethod.GET, RequestMethod.POST})
+	public String wform() {
+		//System.out.println("PhonebookController.wform()");
+		 
+		return "writeForm";
 	}
+	
+	@RequestMapping(value ="write", method = {RequestMethod.GET, RequestMethod.POST})
+	public String write(@ModelAttribute PhonebookVO phonebookVO) {
+		//System.out.println("PhonebookController.write()");
+		
+		System.out.println(phonebookVO);
+		
+		PhonebookDAO phonebookDAO = new PhonebookDAO();
+		phonebookDAO.phonebookInsert(phonebookVO);
+		//System.out.println(count);
+		
+		
+		return "redirect:/list";
+	}
+	
+	@RequestMapping(value= "/delete", method= {RequestMethod.GET, RequestMethod.POST})
+	public String delete(@RequestParam(value="no")int no) {
+		System.out.println("PhonebookController.delete()");
+		
+		System.out.println(no);
+		
+		PhonebookDAO phonebookDAO = new PhonebookDAO();
+		phonebookDAO.phonebookDelete(no);
+		
+		
+		
+		return "redirect/list";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
--- src/main/webapp/WEB-INF/views/list.jsp
+++ src/main/webapp/WEB-INF/views/list.jsp
@@ -1,16 +1,59 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
 
 <!DOCTYPE html>
 
 <html>
-
 	<head>
 		<meta charset="UTF-8">
 		<title>Insert title here</title>
 	</head>
 	
 	<body>
+		<h1>주소록</h1>
+		
+		<h2>전화번호 리스트</h2>
+		<p>전화번호 리스트 입니다.</p>
+		
+			
+		
+		<c:forEach items="${requestScope.pList}" var ="phonebookVO">
+			<table border="1">
+				<tbody>
+					<tr>
+						<td>이름(name)</td>
+						<td>${phonebookVO.name}</td>
+					<tr>
+					<tr>
+						<td>핸드폰(hp)</td>
+						<td>${phonebookVO.hp}</td>
+					<tr>
+					<tr>
+						<td>회사(company)</td>
+						<td>${phonebookVO.company}</td>
+					<tr>	
+					<tr>
+						<td>
+							<a href="http://localhost:8888/phonebook4/mform&personId=${personVO.personId}">
+								[수정폼으로 이동]
+							</a>		
+						</td>
+						<td>
+							<a href="${pageContext.request.contextPath}/delete&no=${phonebookVO.personId}">
+								[삭제] ${phonebookVO.personId}
+							</a>
+						</td>   <!-- 원래 버튼으로 해야된다 js필요 안배워서 a 태그로 구현 -->
+					<tr>	
+						
+				</tbody>
+			</table>  
+			<br>
+		</c:forEach>
+		
+		
+		
+		<a href="${pageContext.request.contextPath}/wform">등록폼 이동</a>
 	
 	</body>
-
 </html>
